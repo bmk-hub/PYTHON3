f8bbc2a684b7d7ef9127cee7f8aad1151fd90c62
Briankyalo <Expertcarter7@gmail.com.>
2026-01-09 11:59:04 +0300

    I am adding this to the python3 repo.

---
+++ practice_functions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def networking_protocols(Ip_address, port):
+	print(f"Hello, my ip address is {Ip_address} and my port is {port} .")
+	
+Ip_address = input("Enter Ip_address: ")
+port = int(input("Enter port: "))
+
+networking_protocols(Ip_address, port)
+
+
+
