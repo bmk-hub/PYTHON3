29326fc459e9ddedc3b053fbc5b8d8f25f2766af
Briankyalo <Expertcarter7@gmail.com.>
2026-01-14 15:21:26 +0300

    study of for loops

---
+++ for_loops.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+ports = [22, 80, 433, 8080]
+
+for port in ports:
+	print("Checking ports: ", port)
+	
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+    
+for ch in "Brian":
+	print(ch)
+	 
+for i in range(1, 6):
+	print(i)
+	
+for i in range(10, 0, -1):
+	print(i)
+	
+for i in range(10):
+	print(i)
+	
