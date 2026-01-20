1afefdc4783916382b5a797a672d7787480e6875
bmk-hub <Expertcarter7@gmail.com>
2026-01-21 02:08:33 +0300

    Update os_practice.py
    added the last option to specify searching for .py files

--- os_practice.py
+++ os_practice.py
@@ -11,7 +11,11 @@ if os.path.exists(file_path):
 	print("This file exists in the python3 directory.")
 else:
 	print("This file does not exist in the python3 directory.")
-	
+entries = os.listdir("/home/bmk/python3")
+
+for name in entries:
+	if name.endswith(".py"):
+		print("Python file:", name)	
 	
 	
 	
